c70843e9e8aabc7a1e00ff012b48ff2a282eb6bb
HyegyuMoon <nationalv@naver.com>
2022-09-20 23:52:37 +0900

    Basic Static Function

---
+++ 002. Basic Static Function/02. Standard Deviation function/Stdev Example 02.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = [1, 2], [3, 4]
+
+print(np.std(a, axis=0))
+print(np.std(a, axis=1))
---
+++ 002. Basic Static Function/03. Variance Function/Var Example 02.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = [1, 2], [3, 4]
+
+print(np.var(a, axis=0))
+print(np.var(a, axis=1))

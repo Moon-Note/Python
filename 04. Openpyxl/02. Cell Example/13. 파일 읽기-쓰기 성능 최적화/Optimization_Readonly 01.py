f98d959908da924a368f08b42e5d08026f759295
MoonNote <nationalv@kakao.com>
2025-05-07 00:23:41 +0900

    Openpyxl

--- 04. Openpyxl/02. Cell Example/13. 파일 읽기-쓰기 성능 최적화/Optimization_Readonly 01.py
+++ 04. Openpyxl/02. Cell Example/13. 파일 읽기-쓰기 성능 최적화/Optimization_Readonly 01.py
@@ -1,6 +1,12 @@
 from openpyxl import load_workbook
+import os
 
-wb = load_workbook("large_file.xlsx", read_only=True)
+# You must change the physical path before running this script.
+currPath = os.getcwd()+"/04. Openpyxl/02. Cell Example/"
+# # # Check the current path
+print("Current Path:", currPath)
+
+wb = load_workbook(filename=currPath+"large_file.xlsx", read_only=True)
 ws = wb.active
 
 for row in ws.iter_rows(min_row=1, max_col=3, max_row=10):
